519198cde9ba6407b6d49505db490cf52555aebb
Joe Bupdista <129858400+17joaopimenta@users.noreply.github.com>
2025-02-27 14:00:29 +0000

    Adicionei um sistema de verificacao de cartas que determina qual sera a ganhadora

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -29,30 +30,45 @@ int main() {
     float densidade1;
     //Pib per capita do local
     float pibPerCapita1;
-
+    
+    //Calculo do super poder (soma de todas as variaveis ja setadas)
+    float supePoder1;
+    
     //Carta 2
     //Variavel que armazena as letras dos oito primeiros estados (A a H)
     char estado2;
     /*
-        Codigo Carta: Armazenar o codigo da carta (inicial do nome do estado mais um numero de 01 a 04)
-        Nome Cidade: Armazenar o nome atual da cidade
+    Codigo Carta: Armazenar o codigo da carta (inicial do nome do estado mais um numero de 01 a 04)
+    Nome Cidade: Armazenar o nome atual da cidade
     */
     char codigoCarta2[50], nomeCidade2[50];
     /*
-        Populacao: Numero total da populacao
-        Quantidade Pontos Turisticos: Numero de pontos turisticos
+    Populacao: Numero total da populacao
+    Quantidade Pontos Turisticos: Numero de pontos turisticos
     */
     int populacao2, quantidadePontosTuristicos2;
     /*
-        Area: area da cidade
-        PIB: PIB da cidade
+    Area: area da cidade
+    PIB: PIB da cidade
     */
     float area2, pib2;
-    
+ 
     //Densidade demografica
     float densidade2;
     //Pib per capita do local
     float pibPerCapita2;
+    
+    //Calculo do super poder (soma de todas as variaveis ja setadas)
+    float supePoder2;
+ 
+    //Vitorias das cartas (Strings que serao usadas em uma estrutura condicional que armazenarão as cartas vencedoras)
+    char vitoriaPopulacao[30];
+    char vitoriaPontosTuristicos[30];
+    char vitoriaArea[30];
+    char vitoriaPib[30];
+    char vitoriaDensidade[30];
+    char vitoriaPibPerCapta[30];
+    char vitoriaSuperPoder[30];
 
     //Carta1 (Documentacoes que o usuario ira colocar)
     printf("Carta1\n");
@@ -80,7 +96,10 @@ int main() {
 
     densidade1 = populacao1/area1;
     pibPerCapita1 = (pib1*1e9)/(float)populacao1;
-
+    //Calculo do Super Poder (soma de todos os valores numéricos que foram colocados como informacao nas cartas)
+    //A densidade foi elevada a -1 visto que ela é inversamente proporcional
+    supePoder1 = populacao1 + quantidadePontosTuristicos1 + area1 + pib1 + 1/densidade1 + pibPerCapita1;
+    
     //Carta2 (Documentacoes que o usuario ira colocar)
     printf("\nCarta2\n");
     
@@ -95,22 +114,51 @@ int main() {
     
     printf("Populacao: ");
     scanf("%i", &populacao2);
-
+    
     printf("Quantidade de pontos turisticos: ");
     scanf("%i", &quantidadePontosTuristicos2);
-
+    
     printf("Area: ");
     scanf("%f", &area2);
-
+    
     printf("PIB: ");
     scanf("%f", &pib2);
-
+    
     densidade2 = populacao2/area2;
     pibPerCapita2 = (pib2*1e9)/populacao2;
+    supePoder2 = populacao2 + quantidadePontosTuristicos2 + area2 + pib2 + 1/densidade2 + pibPerCapita2;
+    
+    //Variaveis para exibir o resultado da disputa
+    
+    if (populacao1 > populacao2) strcpy(vitoriaPopulacao,"Carta1");
+    else strcpy(vitoriaPopulacao,"Carta2");   
+    
+    if (area1 > area2) strcpy(vitoriaArea,"Carta1");
+    else strcpy(vitoriaArea,"Carta2");
 
-    //Escrever os valores de cada carta
-    printf("\nCarta1\nEstado: %c\nCodigo: %s\nNome da Cidade: %s\nPopulacao: %i\nQuantidade de pontos turisticos: %i\nArea: %.2fkm²\nPIB: %.2f bilhoes de reais\nDensidade Populacional: %.2fhab/km²\nPIB per Capita: %.2f reais\n",estado1,codigoCarta1,nomeCidade1,populacao1,quantidadePontosTuristicos1,area1,pib1,densidade1, pibPerCapita1);
-    printf("\nCarta2\nEstado: %c\nCodigo: %s\nNome da Cidade: %s\nPopulacao: %i\nQuantidade de pontos turisticos: %i\nArea: %.2fkm²\nPIB: %.2f bilhoes de reais\nDensidade Populacional: %.2fhab/km²\nPIB per Capita: %.2f reais\n",estado2,codigoCarta2,nomeCidade2,populacao2,quantidadePontosTuristicos2,area2,pib2,densidade2, pibPerCapita2);
+    if (pib1 > pib2) strcpy(vitoriaPib,"Carta1");
+    else strcpy(vitoriaPib,"Carta2");   
+    
+    if (quantidadePontosTuristicos1 > quantidadePontosTuristicos2) strcpy(vitoriaPontosTuristicos,"Carta1");
+    else strcpy(vitoriaPontosTuristicos,"Carta2");
 
+    if (densidade1 < densidade2) strcpy(vitoriaDensidade,"Carta1");
+    else strcpy(vitoriaDensidade,"Carta2");   
+    
+    if (pibPerCapita1 > pibPerCapita2) strcpy(vitoriaPibPerCapta,"Carta1");
+    else strcpy(vitoriaPibPerCapta,"Carta2");
+
+    if (supePoder1 > supePoder2) strcpy(vitoriaSuperPoder,"Carta1");
+    else strcpy(vitoriaSuperPoder,"Carta2");
+
+    //Escrever a situação de vitória das cartas
+    printf("\nComparação entre cartas\n");
+    printf("Populacao: %s venceu (%d)\n", vitoriaPopulacao, populacao1 > populacao2);
+    printf("Area: %s venceu (%d)\n", vitoriaArea, area1 > area2);
+    printf("Pib: %s venceu (%d)\n", vitoriaPib, pib1 > pib2);
+    printf("Pontos Turisticos: %s venceu (%d)\n", vitoriaPontosTuristicos, quantidadePontosTuristicos1 > quantidadePontosTuristicos2);
+    printf("Densidade: %s venceu (%d)\n", vitoriaDensidade, densidade1 < densidade2);
+    printf("PIB per Capita: %s venceu (%d)\n", vitoriaPibPerCapta, pibPerCapita1 > pibPerCapita2);
+    printf("Super Poder: %s venceu (%d)\n", vitoriaSuperPoder, supePoder1 > supePoder2);
     return 0;
 }
